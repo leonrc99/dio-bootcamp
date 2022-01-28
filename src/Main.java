63fa7c015268cf162ae68e326dbdca68ab6ffca6
Leonardo <leonardo.rc999@gmail.com>
2022-01-27 22:15:05 -0300

    Created class "Mentoria" and "Cursos"

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import com.dio.bootcamp.dominio.Curso;
+import com.dio.bootcamp.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args){
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java Básico");
+        curso1.setDescricao("Curso de Java básico.");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Introdução a Orientação a Objetos");
+        curso2.setDescricao("Neste curso faremos uma introdução ao paradigma de Orientação à Objetos.");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Sobre o Java");
+        mentoria.setDescricao("Vamos falar e tirar dúvidas sobre o Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria);
+        System.out.println(curso2);
+    }
+}
