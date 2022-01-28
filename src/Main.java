2fc780e709977e59cda9cbedeadb70d09ac98253
Leonardo <leonardo.rc999@gmail.com>
2022-01-27 23:28:45 -0300

    Create class "Dev", "Bootcamp" and "Conteudo"

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,17 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args){
         Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+        Mentoria mentoria = new Mentoria();
+
         curso1.setTitulo("Java Básico");
         curso1.setDescricao("Curso de Java básico.");
         curso1.setCargaHoraria(8);
 
-        Curso curso2 = new Curso();
         curso2.setTitulo("Introdução a Orientação a Objetos");
         curso2.setDescricao("Neste curso faremos uma introdução ao paradigma de Orientação à Objetos.");
         curso2.setCargaHoraria(4);
 
-        Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Sobre o Java");
         mentoria.setDescricao("Vamos falar e tirar dúvidas sobre o Java");
         mentoria.setData(LocalDate.now());
